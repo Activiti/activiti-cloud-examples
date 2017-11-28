b9a50720217f3891b2df372b99e62398e6b4e57a
Salaboy <salaboy@gmail.com>
2017-11-28 15:09:09 +0000

    adding initial kubernetes desc

--- twitter-processor-example/activiti-cloud-connectors-twitter/src/main/java/org/activiti/cloud/connectors/twitter/Twitter4JConnector.java
+++ twitter-processor-example/activiti-cloud-connectors-twitter/src/main/java/org/activiti/cloud/connectors/twitter/Twitter4JConnector.java
@@ -35,8 +35,7 @@ public class Twitter4JConnector {
         System.out.println(" I'm tweeting to a Winner: " + event.getVariables().get("winner") + " \n");
         System.out.println("#################################################################################");
 
-        IntegrationResultEvent ire = new IntegrationResultEvent(UUID.randomUUID().toString(),
-                                                                event.getExecutionId(),
+        IntegrationResultEvent ire = new IntegrationResultEvent(event.getExecutionId(),
                                                                 results);
 
       //  System.out.println("I'm sending back an integratrion Result: " + ire);
