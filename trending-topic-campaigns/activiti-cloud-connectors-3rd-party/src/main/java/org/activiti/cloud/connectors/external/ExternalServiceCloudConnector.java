cae2c6d77c2859763ce86890f324f81f1ffac651
Elias Ricken de Medeiros <elias.demedeiros@alfresco.com>
2017-12-14 15:08:08 +0000

    Use configuration properties

--- trending-topic-campaigns/activiti-cloud-connectors-3rd-party/src/main/java/org/activiti/cloud/connectors/external/ExternalServiceCloudConnector.java
+++ trending-topic-campaigns/activiti-cloud-connectors-3rd-party/src/main/java/org/activiti/cloud/connectors/external/ExternalServiceCloudConnector.java
@@ -1,7 +1,23 @@
+/*
+ * Copyright 2017 Alfresco, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.activiti.cloud.connectors.external;
 
+import org.activiti.cloud.connectors.external.config.SLAProperties;
 import org.activiti.cloud.connectors.starter.configuration.EnableActivitiCloudConnector;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,11 +28,11 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan({"org.activiti.cloud.connectors.starter", "org.activiti.cloud.connectors.external"})
 public class ExternalServiceCloudConnector implements CommandLineRunner {
 
-    @Value("${sla.requests}")
-    private int requestPerMinute;
+    private final SLAProperties slaProperties;
 
-    @Value("${sla.enabled}")
-    private boolean slaEnabled;
+    public ExternalServiceCloudConnector(SLAProperties slaProperties) {
+        this.slaProperties = slaProperties;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ExternalServiceCloudConnector.class,
@@ -25,6 +41,6 @@ public class ExternalServiceCloudConnector implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Starting SLA Requests with: RPM -> " + requestPerMinute + " and Enabled: " + slaEnabled);
+        System.out.println("Starting SLA Requests with: RPM -> " + slaProperties.getRequests() + " and Enabled: " + slaProperties.isEnabled());
     }
 }
