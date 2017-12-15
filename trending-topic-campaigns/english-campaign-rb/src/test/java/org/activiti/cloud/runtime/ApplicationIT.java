9a6f3d501952f6703d769f12eb32874df62df98a
Elias Ricken de Medeiros <elias.demedeiros@alfresco.com>
2017-12-15 16:23:43 +0000

    Clean up test

--- trending-topic-campaigns/english-campaign-rb/src/test/java/org/activiti/cloud/runtime/ApplicationIT.java
+++ trending-topic-campaigns/english-campaign-rb/src/test/java/org/activiti/cloud/runtime/ApplicationIT.java
@@ -2,8 +2,9 @@ package org.activiti.cloud.runtime;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -14,15 +15,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DirtiesContext
 public class ApplicationIT {
 
-    @Value("${spring.activiti.process-definition-location-prefix:}")
-    private String procPath;
+    @Autowired
+    private ApplicationContext context;
 
-    //for test purposes the env variable ACT_RB_PROCESSES_PATH is as set in pom.xml using surefire
     @Test
     public void contextLoads() throws Exception {
-        //check variable has been resolved for path to processes
-        //first check it's not the default
-        assertThat(procPath).isNotEqualTo("file:/processes/");
-        assertThat(procPath).isEqualTo("file:" + System.getenv("ACT_RB_PROCESSES_PATH"));
+        assertThat(context).isNotNull();
     }
 }
