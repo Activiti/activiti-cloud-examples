b3e0e2f7eadf764181bcaf7786eb8c7004ebf13d
Salaboy <salaboy@gmail.com>
2018-02-05 17:10:03 +0000

    remove trending topic campaigns example, moved to its own repo

--- trending-topic-campaigns/activiti-cloud-connectors-dummytwitter/src/main/java/org/activiti/cloud/connectors/twitter/TweetRepository.java
+++
@@ -1,7 +0,0 @@
-package org.activiti.cloud.connectors.twitter;
-
-import org.activiti.cloud.connectors.twitter.model.TweetEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TweetRepository extends CrudRepository<TweetEntity, Long> {
-}
