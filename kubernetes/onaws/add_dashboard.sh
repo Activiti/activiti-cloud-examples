ddaf7a1535e42828dafa75fa5d5c37b33d1af62f
salaboy <salaboy@gmail.com>
2018-08-08 18:29:27 +0100

    removing k8s descriptors, look at our activiti-cloud-charts

--- kubernetes/onaws/add_dashboard.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-kubectl=$(which kubectl)
-
-export VERSION=v1.6.3
-
-$kubectl create -f https://raw.githubusercontent.com/kubernetes/kops/master/addons/kubernetes-dashboard/${VERSION}.yaml
